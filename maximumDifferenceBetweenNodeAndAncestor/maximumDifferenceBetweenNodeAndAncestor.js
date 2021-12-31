13145bae6fb1329fba3a0acfd8214489d10f31b0
Taniel Pogharian <taniel.pogharian@gmail.com>
2021-12-31 13:43:26 -0700

    Complete

---
+++ maximumDifferenceBetweenNodeAndAncestor/maximumDifferenceBetweenNodeAndAncestor.js
@@ -0,0 +1,20 @@
+// Definition for a binary tree node.
+// function TreeNode(val, left, right) {
+//   this.val = (val === undefined ? 0 : val)
+//   this.left = (left === undefined ? null : left)
+//   this.right = (right === undefined ? null : right)
+// }
+
+var maxAncestorDiff = function (root) {
+  function dfs(node, max, min) {
+    if (!node) {
+      return max - min;
+    }
+
+    max = Math.max(max, node.val);
+    min = Math.min(min, node.val);
+
+    return Math.max(dfs(node.left, max, min), dfs(node.right, max, min));
+  }
+  return dfs(root, root.val, root.val)
+};
