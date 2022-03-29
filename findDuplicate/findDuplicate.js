f0524c34d72241edd87f6690e295324459d7f563
Taniel Pogharian <taniel.pogharian@gmail.com>
2022-03-29 12:27:35 -0700

    Complete

---
+++ findDuplicate/findDuplicate.js
@@ -0,0 +1,20 @@
+// Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+// There is only one repeated number in nums, return this repeated number.
+
+// You must solve the problem without modifying the array nums and uses only constant extra space.
+
+var findDuplicate = function (nums) {
+  let obj = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    if (obj[nums[i]] === undefined) {
+      obj[nums[i]] = 1;
+    } else {
+      obj[nums[i]]++;
+      if (obj[nums[i]] === 2) {
+        return nums[i];
+      }
+    }
+  }
+};
